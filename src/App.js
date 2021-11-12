690607c0f6196a1cf93d169b00f92ef300b51513
marmon179 <marmon179@gmail.com>
2021-11-12 15:38:56 +0300

    Task 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let selValue = React.createRef();
+    let inValue = React.createRef();
+    let inRange = React.createRef();
+    let inNumber = React.createRef();
+
+    let [classQuare, setClassQuare] = useState("task-2")
+    const [check, setCheck] = useState();
+    const [output, setOutput] = useState();
+    const [colorNow, setColor] = useState();
+    const [input, setInput] = useState();
+    const [inrange, setRange] = useState();
+
+
+    const task1 = () => {
+        console.log("task2")
+    }
+    const task2 = () => {
+        setClassQuare("task-2 active")
+    }
+    const task3 = (e) => {
+        console.log(e.target.value)
+    }
+    const task4 = () => {
+        let count = 0;
+        return () => console.log(count++)
+    }
+    const task5 = (e) => {
+        let value = e.target.value;
+        e.target.checked ? setCheck(value) : setCheck("")
+    }
+    const task6 = () => {
+        setOutput(selValue.current.value)
+    }
+    const task7 = () => {
+        let min = 0;
+        let max = 255;
+        let r = Math.floor(Math.random() * (max - min + 1)) + min;
+        let g = Math.floor(Math.random() * (max - min + 1)) + min;
+        let b = Math.floor(Math.random() * (max - min + 1)) + min;
+        let color = `rgb(${r}, ${g}, ${b})`
+        setColor(color)
+    };
+    const task8 = () => {
+        let strValue = inValue.current.value;
+        let last = strValue.charAt(strValue.length - 1);
+        let reg = /^\d+$/;
+        if (!reg.test(last)) {
+            setInput("1")
+        } else {
+            setInput("0")
+        }
+    };
+    const task9 = () => {
+        setRange(inRange.current.value)
+    };
+
+    let ar10 = [5, 6, 7];
+
+    function task10() {
+        ar10.push(+inNumber.current.value)
+        console.log(ar10);
+    }
+
+    return (
+        <>
+            <section>
+                <h2>Task 1</h2>
+                <button onClick={task1}>Task 1</button>
+            </section>
+            <section>
+                <h2>Task 2</h2>
+                <div onMouseEnter={task2} className={classQuare}></div>
+            </section>
+            <section>
+                <h2>Task 3</h2>
+                <input onChange={task3} type="text" className="task-3"/>
+            </section>
+            <section>
+                <h2>Task 4</h2>
+                <button onClick={task4()} className="task-4">Count</button>
+            </section>
+            <section>
+                <h2>Task 5</h2>
+                <input onChange={task5} type="checkbox" value="55"/>
+                <div className="out-5">{check}</div>
+            </section>
+            <section>
+                <h2>Task 6</h2>
+                <select onChange={task6} className="task-6" ref={selValue}>
+                    <option value="7">seven</option>
+                    <option value="4">four</option>
+                    <option value="9">nine</option>
+                    <option value="10">ten</option>
+                </select>
+                <div className="out-6">{output}</div>
+            </section>
+            <section>
+                <h2>Task 7</h2>
+                <div style={{background: colorNow}} className="block-7"></div>
+                <button onClick={task7} className="task-7">Color</button>
+            </section>
+            <section>
+                <h2>Task 8</h2>
+                <input onKeyPress={task8} type="text" className="task-8" ref={inValue}></input>
+                <div className="out-8">{input}</div>
+            </section>
+            <section>
+                <h2>Task 9</h2>
+                <input onInput={task9} type="range" className="task-9" ref={inRange}></input>
+                <div className="out-9">{inrange}</div>
+            </section>
+            <section>
+                <h2>Task 10</h2>
+                <input type="number" className="i-10" ref={inNumber}></input>
+                <button onClick={task10} className="task-10">Push</button>
+            </section>
+        </>
+    );
 }
 
 export default App;
